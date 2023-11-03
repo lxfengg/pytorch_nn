d240bf37c2168639db8946c856cc410c526e78cb
liuxiaofeng <liuxiaofeng@qlcd.com>
2023-11-03 11:09:09 +0800

    vgg16

--- src/module_pretrained.py
+++ src/module_pretrained.py
@@ -1,7 +1,14 @@
 import torchvision.models
+from torch import nn
 
 vgg_16_false = torchvision.models.vgg16()
 vgg_16_true = torchvision.models.vgg16(weights=torchvision.models.VGG16_Weights.DEFAULT)
 
 print(vgg_16_false)
+
+# vgg_16_true.add_module("add", nn.Linear(1000, 10))
+vgg_16_true.classifier.add_module("add", nn.Linear(1000, 10))
 print(vgg_16_true)
+
+vgg_16_false.classifier[6] = nn.Linear(1000, 10)
+print(vgg_16_false)
